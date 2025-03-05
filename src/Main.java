f5975a00c979d9baee9a2e76dabd52c1d2646e96
Bhaskar872kumar <kumarbhaskar872@gmail.com>
2025-03-05 17:19:32 +0530

    .executeQuery(original query in string)

--- src/Main.java
+++ src/Main.java
@@ -4,18 +4,13 @@ public class Main {
     Class.forName("com.mysql.cj.jdbc.Driver");
     Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/DatabaseForJdbc", "root", "mysql@872Z");
     Statement st = con.createStatement();
-        String query = "INSERT INTO students VALUES (7, 'gopal'),(8,'balkrishna')";
-//    ResultSet rs = st.executeQuery(query); //DQL-data query language returns object
-        int count= st.executeUpdate(query);
-
-
-
-//    while (rs.next()) {
-//        String name = rs.getString(2) + " " + rs.getInt("id");
-//        System.out.println(name);
-//    }
-        System.out.println(count);
-st.close();
+        String query = "select * from students";
+        ResultSet rs = st.executeQuery(query); //DQL-data query language returns object
+    while (rs.next()) {
+        String name = rs.getString(2) + " " + rs.getInt("id");
+        System.out.println(name);
+    }
+    st.close();
     con.close();
     }
 }
