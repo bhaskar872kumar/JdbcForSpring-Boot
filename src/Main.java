6727210f75938c069ba3728060b5061d836fdb3f
Bhaskar872kumar <kumarbhaskar872@gmail.com>
2025-03-05 18:00:57 +0530

    Prepared Statement

--- src/Main.java
+++ src/Main.java
@@ -3,14 +3,11 @@ public class Main {
     public static void main(String[] args) throws Exception {
     Class.forName("com.mysql.cj.jdbc.Driver");
     Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/DatabaseForJdbc", "root", "mysql@872Z");
-    Statement st = con.createStatement();
-        String query = "select * from students";
-        ResultSet rs = st.executeQuery(query); //DQL-data query language returns object
-    while (rs.next()) {
-        String name = rs.getString(2) + " " + rs.getInt("id");
-        System.out.println(name);
-    }
-    st.close();
+    PreparedStatement ps = con.prepareStatement("insert into students values(?,?)");
+    ps.setInt(1,34);
+    ps.setString(2,"Diwakar");
+        System.out.println(ps.executeUpdate());
+    ps.close();
     con.close();
     }
 }
